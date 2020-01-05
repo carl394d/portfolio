0743700b22f2dbaf62ef79114630245ac13181ec
carl394d <carl394d@stud.kea.dk>
2020-01-05 18:18:23 +0100

    fixed burger menu and made gallery

--- script.js
+++ script.js
@@ -1,10 +1,31 @@
-window.addEventListener("load", load);
+window.addEventListener("load", sidenVises);
 
-function load() {
-    console.log("sidenvises");
-    document.querySelector("#menuknap").addEventListener("click", burgerMenu);
+function sidenVises() {
+    console.log("sidenVises");
+    document.querySelector("#menuknap").addEventListener("click", toggleMenu);
 }
 
-function burgerMenu() {
+function toggleMenu() {
+    console.log("toggleMenu");
     document.querySelector("#menu").classList.toggle("hidden");
+
+
+
+}
+
+var slideIndex = 0;
+carousel();
+
+function carousel() {
+    var i;
+    var x = document.getElementsByClassName("mySlides");
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > x.length) {
+        slideIndex = 1
+    }
+    x[slideIndex - 1].style.display = "block";
+    //setTimeout(carousel, 2000); // Change image every 2 seconds
 }
